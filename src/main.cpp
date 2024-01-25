101391c757e51c42d6d2adb528ce092b6743028d
CosmicBagel <>
2024-01-24 20:43:40 -0700

    main.ccp tidy up

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,26 @@
 #include <iostream>
 
 int main() {
-    std::cout << "template project doth print" << std::endl;
+    std::cout << "oh hey it pong" << std::endl;
 
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 600;
+    const int targetFps = 60;
 
-    InitWindow(screenWidth, screenHeight,
-               "raylib [core] example - basic window");
+    InitWindow(screenWidth, screenHeight, "cpp_pong");
 
-    SetTargetFPS(60);
+    SetTargetFPS(targetFps);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20,
-                 LIGHTGRAY);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
     }
 
     return 0;
 }
+
