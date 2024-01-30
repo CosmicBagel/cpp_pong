5b6526999742af223a4f62ef8f1653b9fe390645
CosmicBagel <>
2024-01-30 01:23:12 -0700

    Mocked out a simple gameobject architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,79 @@
 #include <raylib.h>
 
+#include <format>
 #include <iostream>
+#include <memory>
+#include <vector>
+
+class IGameObject {
+   public:
+    IGameObject() = default;
+    IGameObject(const IGameObject &) = default;
+    IGameObject(IGameObject &&) = default;
+    IGameObject &operator=(const IGameObject &) = default;
+    IGameObject &operator=(IGameObject &&) = default;
+
+    virtual ~IGameObject() = default;
+
+    virtual void Init() = 0;
+    virtual void Draw() = 0;
+    virtual void Update() = 0;
+};
+
+struct TransformComponent final {
+    int x = 0;
+    int y = 0;
+};
+
+class PaddleGraphicComponent final {
+   public:
+    void Draw(TransformComponent transform) const {
+        DrawRectangle(transform.x, transform.y, 100, 100, BLACK);
+    }
+};
+
+class PlayerPaddle final : public IGameObject {
+   private:
+    uint count = 0;
+    PaddleGraphicComponent paddleGraphic;
+    TransformComponent transform;
+
+   public:
+    void Init() override {
+        transform.x = 400;
+        transform.y = 200;
+    }
+    void Draw() override {
+        paddleGraphic.Draw(transform);
+        auto str = std::format("{}", count);
+        DrawText(str.c_str(), 0, 0, 18, BLACK);
+    }
+    void Update() override { count += 1; }
+};
+
+class ComputerPaddle final : public IGameObject {
+   private:
+    PaddleGraphicComponent paddleGraphic;
+    TransformComponent transform;
+
+   public:
+    void Init() override {
+        transform.x = 100;
+        transform.y = 200;
+    }
+    void Draw() override { paddleGraphic.Draw(transform); }
+    void Update() override {}
+};
+
+class Ball final : public IGameObject {
+   private:
+    TransformComponent transform;
+
+   public:
+    void Init() override { transform = {250, 250}; }
+    void Draw() override { DrawCircle(transform.x, transform.y, 10, RED); }
+    void Update() override {}
+};
 
 int main() {
     std::cout << "oh hey it pong" << std::endl;
@@ -13,16 +86,27 @@ int main() {
 
     SetTargetFPS(targetFps);
 
+    std::vector<std::unique_ptr<IGameObject>> gameObjects;
+    gameObjects.push_back(std::unique_ptr<IGameObject>{new PlayerPaddle});
+    gameObjects.push_back(std::unique_ptr<IGameObject>{new ComputerPaddle});
+    gameObjects.push_back(std::unique_ptr<IGameObject>{new Ball});
+
+    for (auto &gobj : gameObjects) {
+        gobj->Init();
+    }
+
     while (!WindowShouldClose()) {
+        for (auto &gobj : gameObjects) {
+            gobj->Update();
+        }
+
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
+        for (auto &gobj : gameObjects) {
+            gobj->Draw();
+        }
         EndDrawing();
     }
 
     return 0;
 }
-
