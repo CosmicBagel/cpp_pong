c3a22579244e2cb4759fdd34871c97ad038953d8
CosmicBagel <>
2024-02-09 12:11:14 -0700

    Added rect physics object to ball

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,7 +1,12 @@
 #include "Ball.hpp"
 
+#include "PhysicsSystem.hpp"
 #include "raylib.h"
 
-void Ball::Init() { transform = {250, 250}; }
+void Ball::Init() {
+    transform = {250, 250};
+    // initialize
+    rectanglePhysicsObjectId = PhysicsSystem::GetInstance().CreateRectangle();
+}
 void Ball::Draw() { DrawCircle(transform.x, transform.y, 10, RED); }
 void Ball::Update() {}
--- src/Ball.hpp
+++ src/Ball.hpp
@@ -2,10 +2,12 @@
 
 #include "IGameObject.hpp"
 #include "TransformComponent.hpp"
+#include "PoolManager.hpp"
 
 class Ball final : public IGameObject {
    private:
     TransformComponent transform;
+    PoolObjectId rectanglePhysicsObjectId;
 
    public:
     void Init() override;
