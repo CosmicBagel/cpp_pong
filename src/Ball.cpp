16e0fb119c0fa57239d207df9c94710dbf9e439f
CosmicBagel <>
2024-02-12 21:27:38 -0700

    Upgraded Ball with new collider component

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -1,50 +1,25 @@
 #include "Ball.hpp"
 
-#include "Crash.hpp"
 #include "PhysicsSystem.hpp"
+#include "PoolManager.hpp"
 #include "RectanglePhysicsObject.hpp"
 #include "raylib.h"
 
 PhysicsSystem& physicsSystem = PhysicsSystem::GetInstance();
 
-void Ball::Init() {
-    transform = {250, 250};
-    // initialize
-    rectanglePhysicsObjectId = physicsSystem.CreateRectangle(transform, 20, 20);
-}
+void Ball::Init() {}
 
 void Ball::Draw() { DrawRectangle(transform.x, transform.y, 20, 20, drawColor); }
 
 void Ball::Update() {
-    RectanglePhysicsObject& physObj = physicsSystem.GetRectangle(rectanglePhysicsObjectId);
-
-    if (!physObj.OverlappingObjects.empty()) {
+    drawColor = RED;
+    collider.CheckIntersections([&](PoolObjectId otherId) {
         drawColor = GREEN;
-    } else {
-        drawColor = RED;
-    }
+        velX *= -1;
+        velY *= -1;
+    });
 
-    int speed = 1;
-    if (IsKeyDown(KEY_LEFT_SHIFT)) {
-        speed = 3;
-    }
-
-    int xDiff = 0;
-    int yDiff = 0;
-    if (IsKeyDown(KEY_W)) {
-        yDiff -= speed;
-    }
-    if (IsKeyDown(KEY_S)) {
-        yDiff += speed;
-    }
-    if (IsKeyDown(KEY_D)) {
-        xDiff += speed;
-    }
-    if (IsKeyDown(KEY_A)) {
-        xDiff -= speed;
-    }
-
-    transform.x += xDiff;
-    transform.y += yDiff;
-    physObj.SetPos(transform);
+    transform.x += velX;
+    transform.y += velY;
+    collider.UpdatePosition(transform);
 }
--- src/Ball.hpp
+++ src/Ball.hpp
@@ -2,15 +2,21 @@
 
 #include "IGameObject.hpp"
 #include "PoolManager.hpp"
+#include "RectangleColliderComponent.hpp"
 #include "TransformComponent.hpp"
 
 class Ball final : public IGameObject {
    private:
     TransformComponent transform;
     PoolObjectId rectanglePhysicsObjectId;
+    RectangleColliderComponent collider;
     Color drawColor = RED;
+    int velX = 1;
+    int velY = 0;
 
    public:
+    Ball() : transform({250, 250}), collider(transform, 20, 20){};
+
     void Init() override;
     void Draw() override;
     void Update() override;
